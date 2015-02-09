83e2309f8d7de0b9e6df7e4e6e4f837cd450d82b
Severed-Infinity <masterdavidswift@hotmail.com>
2015-02-09 10:09:51 +0000

    Initial Commit

---
+++ ADSWeek2Lab2/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  ADSWeek2Lab2
+//
+//  Created by david swift on 09/02/2015.
+//  Copyright (c) 2015 david swift. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
